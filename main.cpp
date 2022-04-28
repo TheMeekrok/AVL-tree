5e3d95875a290a9f8446ffc46a876192202624d3
Artyom <minicoal@gmail.com>
2022-04-28 22:46:15 +1000

    Added left- and right- rotations

--- main.cpp
+++ main.cpp
@@ -18,6 +18,18 @@ void print(Node* node) {
     }
 }
 
+void print_2d(Node* node, int distance, int count) {
+    if (!node) return;
+    distance += count;
+
+    print_2d(node->right, distance, count);
+    cout << std::endl;
+    for (int i = count; i < distance; i++)
+        cout << " ";
+    cout << node->key << " (" << node->height << ")" << endl;
+    print_2d(node->left, distance, count);
+}
+
 int height(Node* node) {
     if (!node) return 0;
     return node->height;
@@ -39,12 +51,28 @@ int get_balance(Node* node) {
     return height(node->left) - height(node->right);
 }
 
-Node* left_rotate(Node* node) {
+Node* left_rotate(Node* x) {
+    Node* y = x->left;
+    Node* T = y->right;
+    x->left = T;
+    y->right = x;
 
+    x->height = max(height(x->left), height(x->left)) + 1;
+    y->height = max(height(y->left), height(y->left)) + 1;
+
+    return y;
 }
 
-Node* right_rotate(Node* node) {
+Node* right_rotate(Node* x) {
+    Node* y = x->right;
+    Node* T = y->left;
+    x->right = T;
+    y->left = x;
 
+    x->height = max(height(x->left), height(x->left)) + 1;
+    y->height = max(height(y->left), height(y->left)) + 1;
+
+    return y;
 }
 
 Node* insert(Node* node, int key) {
@@ -59,22 +87,23 @@ Node* insert(Node* node, int key) {
     node->height = max(height(node->right), height(node->left)) + 1;
 
     int balance = get_balance(node);
-    if (balance > 1) {
-
-    }
+    if (balance > 1)
+        return left_rotate(node);
+    if (balance < -1)
+        return right_rotate(node);
 
     return node;
 }
 
 int main() {
     Node* root = nullptr;
-    root = insert(root, 6);
-    root = insert(root, 5);
     root = insert(root, 10);
-    root = insert(root, 8);
-    root = insert(root, 3);
-    root = insert(root, 0);
+    root = insert(root, 20);
+    root = insert(root, 30);
+    root = insert(root, 40);
+    root = insert(root, 50);
+    root = insert(root, 25);
 
-    //print(root);
+    print_2d(root, 0, 10);
     return 0;
 }
